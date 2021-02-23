2faae27df47e37f18d2aafa0b74922c12d8e2b9b
Maciej Manna <maciejmanna@gmail.com>
2021-02-23 23:24:51 +0100

    added simulation animations; minor fixes

--- visualise.py
+++ visualise.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation, rc
+from IPython.display import HTML
 
 def simple_plot(t, Y):
+  plt.figure(figsize=(15, 10))
+
   plt.plot(t, Y[0], label='x')
   plt.plot(t, Y[1], label='v')
   plt.plot(t, Y[2], label='θ')
@@ -14,7 +18,7 @@ def simple_plot(t, Y):
 
 
 def phase_plots(Y):
-  plt.figure(figsize=(10, 5))
+  plt.figure(figsize=(15, 7))
 
   plt.subplot(221)
   plt.plot(Y[0], Y[1])
@@ -31,3 +35,42 @@ def phase_plots(Y):
   plt.title('Pendulum angle and angular velocity')
 
   plt.show()
+
+'''
+def sim_animation(Y, params):
+  L = params[2]
+
+  plt.figure(figsize=(15, 7))
+
+  plt.grid()
+  plt.xlabel('x')
+
+  fig, ax = plt.subplots()
+
+  ax.set_xlim(( -8, 8))
+  ax.set_ylim(( -0.5, L + 1))
+
+  plots = [plt.plot([],[]), plt.plot([],[]), plt.scatter([],[]), plt.scatter([],[])]
+
+  def init():
+      plots[0].set_data([-0.5, L+1], [0, 0])
+      return plots
+
+  def animate(frame):
+    cart_x = Y[0, frame]
+    cart_y = 0.5
+    head_x = Y[2, frame]
+    head_y = 0.5 + L
+
+    plots[1].set_data([cart_x, head_x], [cart_y, head_y])
+    plots[2].set_data([cart_x], [cart_y])
+    plots[3].set_data([head_x], [head_y])
+
+    return plots
+
+  anim = animation.FuncAnimation(fig, animate, init_func=init, frames=Y.shape[1], interval=20, blit=True)
+  HTML(anim.to_html5_video())
+  rc('animation', html='html5')
+
+  return anim
+'''
